1c67559ce21056b878f6d9ab94ad5b0916063f77
Geovane <geonnave@palantir.(none)>
2013-04-04 00:12:50 -0300

    analisando keywords

--- devel/lib/lexical_analyzer.rb
+++ devel/lib/lexical_analyzer.rb
@@ -1,21 +1,41 @@
-
+require "./keywords.rb"
 
 
 
 class LexicalAnalyzer
 	
 	def line_to_lexemes line
-		line.split!("\s")
-		
+		lexemes = []
+		lex = ""
+		line.split("").each { |c| 
+			lex << c
+			# puts lex
+			if $WORDS.include?(lex) || $OPS.include?(lex) || $SYMBOLS.include?(lex)
+				lexemes << lex 
+				lex = ""
+				# print lex
+				next
+			end
+			# lex.split("").each { |l| 
+			# 	if $OP.include?
+			# }
+			if lex.split("").last == " "# || lex.split("").last == "\n"
+				lex = "" 
+				next
+			end
+		}
+		print lexemes
+		puts
 	end
 
 	def code_to_lexemes file
 		lines = file.split("\n")
 		lexemes = []
-		lines.each do |l|  
+		lines.each_with_index do |l, i|  
+			print i+1
 			lexemes << line_to_lexemes(l)
 		end
-		puts lexemes
+		# puts lexemes
 		lexemes
 	end
 
